503183f352a80c6958d86ac83a68deb1f5525f78
TiniLynxLord787 <97319218+tini-lynxlord787@users.noreply.github.com>
2026-09-19 17:34:33 -0700

    Update print statement from 'Hello' to 'Goodbye'

--- minigameGarden.js
+++ minigameGarden.js
@@ -356,7 +356,7 @@ M.launch=function()
 			'queenbeetLump':{
 				name:'Juicy queenbeet',
 				icon:18,
-				plantable:false,
+				plantable:true,
 				cost:60*2,
 				costM:1000000000000,
 				ageTick:0.04,
